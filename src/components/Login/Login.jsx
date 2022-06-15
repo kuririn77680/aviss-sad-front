3beca366a406ab971f3c8e6c84bda9011ebda79f
Florian  GAGNON <florian-gagnon@aviss-securite.fr>
2022-06-15 18:43:45 +0200

    login feature

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,57 @@
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import './Login.css';
+//
+// async function loginUser(credentials) {
+//  return fetch('http://localhost:8080/login', {
+//    method: 'POST',
+//    headers: {
+//      'Content-Type': 'application/json'
+//    },
+//    body: JSON.stringify(credentials)
+//  })
+//    .then(data => data.json())
+// }
+//
+// export default function Login({ setToken }) {
+//   const [username, setUserName] = useState();
+//   const [password, setPassword] = useState();
+//
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+//     const token = await loginUser({
+//       username,
+//       password
+//     });
+//     setToken(token);
+//   }
+//
+//   return(
+//     <div className="login-wrapper">
+//       <h1>Please Log In</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           <p>Username</p>
+//           <input type="text" onChange={e => setUserName(e.target.value)} />
+//         </label>
+//         <label>
+//           <p>Password</p>
+//           <input type="password" onChange={e => setPassword(e.target.value)} />
+//         </label>
+//         <div>
+//           <button type="submit">Submit</button>
+//         </div>
+//       </form>
+//     </div>
+//   )
+// }
+//
+// Login.propTypes = {
+//   setToken: PropTypes.func.isRequired
+// };
+
 import React, { useRef, useState, useEffect, useContext } from 'react';
 import AuthContext from '../../context/AuthProvider';
-import './Login.css';
 
 import axios from '../../api/axios';
 const LOGIN_URL = '/login';
@@ -25,6 +76,7 @@ const Login = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
+    console.log(LOGIN_URL);
     try {
       const response = await axios.post(LOGIN_URL,
         JSON.stringify({user, password}),
@@ -34,6 +86,7 @@ const Login = () => {
          }
       );
       // console.log(JSON.stringify(response?.data));
+      console.log('test');
       console.log(JSON.stringify(response));
       const accessToken = response.data.accessToken;
       const roles = response.data.roles;
