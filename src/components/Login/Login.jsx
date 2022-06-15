db050a75b8c904ee219398b1bdba3f3a29227761
Florian  GAGNON <florian-gagnon@aviss-securite.fr>
2022-06-15 18:32:17 +0200

    login feature

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,83 +1,101 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Login.css'
+import React, { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from '../../context/AuthProvider';
+import './Login.css';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        username: "",
-        password: ""
-    };
+import axios from '../../api/axios';
+const LOGIN_URL = '/login';
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+const Login = () => {
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, password])
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(LOGIN_URL,
+        JSON.stringify({user, password}),
+        {
+           headers: {'Content-Type': 'application/json'},
+           withCredentials: true
+         }
+      );
+      // console.log(JSON.stringify(response?.data));
+      console.log(JSON.stringify(response));
+      const accessToken = response.data.accessToken;
+      const roles = response.data.roles;
+      setAuth({ user, password, roles, accessToken });
+      setUser('');
+      setPassword('');
+      setSuccess(true);
+    } catch (err) {
+      if (!err.response) {
+        setErrMsg('No Server Response');
+      } else if (err.response.status === 400) {
+        setErrMsg('Missing Username or Password');
+      } else if (err.response.status === 401) {
+        setErrMsg('Unauthorized');
+      } else {
+        setErrMsg('Login Failed');
+      }
+      errRef.current.focus();
+    }
   }
 
-  handleChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  loginUser(credentials){
-    return fetch('http://localhost:8080/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    })
-    .then(data=>data.json())
-  }
-
-  handleSubmit(event) {
-    const {username, password} = this.state;
-    const token = this.loginUser({
-      username,
-      password
-    });
-    this.setState({
-      'token': token
-    });
-  }
-
-  render() {
-    return (
-      <div className="login-wrapper">
-        <h1>Please Log In</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <p>Username</p>
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p ref={errRef} className={errMsg ? "errmsg" :
+          "offscreen"} aria-live="assertive">{errMsg}</p>
+          <h1>Sign in</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
             <input
-              name="username"
               type="text"
-              onChange={this.handleChange}
-              placeholder="E-mail"/>
-          </label>
-          <label>
-            <p>Password</p>
-            <input
-              name="password"
-              type="password"
-              onChange={this.handleChange}
-              placeholder="Password"/>
-          </label>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
 
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired
-};
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+        </section>
+      )}
+      </>
+    )
+}
 
 export default Login
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { AuthProvider } from './context/AuthProvider'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
